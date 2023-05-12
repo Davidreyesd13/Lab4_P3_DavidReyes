65247c54985e849d05af9d48fb3184aa706cc332
Davidreyesd13 <davidreyesd13@gmail.com>
2023-05-12 14:07:46 -0600

    avance ejercicio 1

--- Lab4_P3_DavidReyes.cpp
+++ Lab4_P3_DavidReyes.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 void menu() {
-	std::cout << "hola";
+	int opcion;
+	bool princl = true;
+	while (princl) {
+		cout << "1.Ejercicio 1\n2.Ejericicio 2\n0.Salir";
+		cin >> opcion;
+		switch (opcion) {
+		case 1:break;
+		case 2:
+			break;
+		case 0:
+			princl = false;
+			break;
+		default:
+			cout << "Opcion no valida";
+			break;
+		}
+	}
 }
 int main()
 {
